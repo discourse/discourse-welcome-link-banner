d6253944bd8f59efbece2e7b299fc8d8c6680388
Kelvin Tan <kelv@discourse.org>
2024-12-27 10:18:57 +0800

    DEV: migrate deprecated icon names in settings and update banner_links to objects schema

---
+++ test/unit/migrations/settings/0001-migrate-from-deprecated-icon-names-test.js
@@ -0,0 +1,67 @@
+import { module, test } from "qunit";
+import migrate from "../../../../migrations/settings/0001-migrate-from-deprecated-icon-names";
+
+module(
+  "Unit | Migrations | Settings | 0001-migrate-from-deprecated-icon-names",
+  function () {
+    test("migrate", function (assert) {
+      const settings = new Map(
+        Object.entries({
+          banner_links: JSON.stringify([
+            {
+              icon: "fab-facebook",
+              text: "Facebook",
+              url: "https://www.facebook.com",
+            },
+            { icon: "fab fa-cog", text: "Settings", url: "/settings" },
+            { icon: "user-friends", text: "Groups", url: "not-a-valid-url" },
+          ]),
+          svg_icons: "fab-facebook|fab-twitter|fab-cog|user-friends",
+        })
+      );
+
+      const result = migrate(settings);
+
+      const expectedResult = new Map(
+        Object.entries({
+          banner_links: [
+            {
+              icon: "fab-facebook",
+              text: "Facebook",
+              url: "https://www.facebook.com",
+            },
+            { icon: "fab-gear", text: "Settings", url: "/settings" },
+            { icon: "user-group", text: "Groups", url: "#" },
+          ],
+          svg_icons: "fab-facebook|fab-twitter|fab-gear|user-group",
+        })
+      );
+      assert.deepEqual(Array.from(result), Array.from(expectedResult));
+    });
+
+    test("migrate empty settings", function (assert) {
+      const settings = new Map(Object.entries({}));
+      const result = migrate(settings);
+      assert.deepEqual(Array.from(result), Array.from(settings));
+    });
+
+    test("migrate same settings", function (assert) {
+      const settings = new Map(
+        Object.entries({
+          banner_links: [
+            {
+              icon: "fab-facebook",
+              text: "Facebook",
+              url: "https://www.facebook.com",
+            },
+            { icon: "fab-gear", text: "Settings", url: "/settings" },
+            { icon: "user-group", text: "Groups", url: "/groups" },
+          ],
+          svg_icons: "fab-facebook|fab-twitter|fab-gear|user-group",
+        })
+      );
+      const result = migrate(settings);
+      assert.deepEqual(Array.from(result), Array.from(settings));
+    });
+  }
+);
